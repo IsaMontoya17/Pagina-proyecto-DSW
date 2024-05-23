c8062708190de8d1019f7f2fe356d697f869e454
IsaMontoya17 <isabelamontoya2021@gmail.com>
2024-05-23 15:49:42 -0500

    commit

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -8,3 +8,20 @@ function cambiarImagenes() {
 }
 
 setInterval(cambiarImagenes, 5000);
+
+function validarImagenCuadrada(img) {
+    if (img.naturalWidth !== img.naturalHeight) {
+        if (img.naturalWidth > img.naturalHeight) {
+            img.style.height = 'auto';
+            img.style.width = '20vw';
+            img.style.maxHeight = '20vw';
+        } else {
+            img.style.width = 'auto';
+            img.style.height = '20vw';
+            img.style.maxWidth = '20vw'; 
+        }
+    } else {
+        img.style.width = '20vw';
+    }
+}
+
