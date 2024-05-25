5d1f0f5c294d19880080fcb23dabe62bbd6ee7fd
Sara-Castaneda25 <saraletsbefluent@gmail.com>
2024-05-25 14:10:22 -0500

    Añadiendo pagina mi cuenta

--- js/login.js
+++ js/login.js
@@ -9,23 +9,23 @@ document.addEventListener('DOMContentLoaded', function() {
             const username = document.getElementById('login-username').value;
             const password = document.getElementById('login-password').value;
 
-            // Comprobar si el usuario está registrado en el localStorage
+            
             const storedUser = JSON.parse(localStorage.getItem('user'));
             if (storedUser && storedUser.username === username && storedUser.password === password) {
-                // Usuario autenticado
+               
                 console.log('Inicio de sesión exitoso');
                 alert('Inicio de sesión exitoso');
                 // Limpiar campos
                 document.getElementById('login-username').value = '';
                 document.getElementById('login-password').value = '';
             } else {
-                // Usuario no autenticado
+                
                 console.log('Usuario o contraseña incorrectos');
                 alert('Usuario o contraseña incorrectos');
             }
         });
 
-        // Manejar el evento del botón "Mostrar contraseña" para inicio de sesión
+       
         const showPasswordCheckbox = document.getElementById('login-show-password');
         if (showPasswordCheckbox) {
             showPasswordCheckbox.addEventListener('change', function() {
@@ -46,13 +46,13 @@ document.addEventListener('DOMContentLoaded', function() {
             const password = document.getElementById('register-password').value;
             const confirmPassword = document.getElementById('confirm-password').value;
 
-            // Verificar si las contraseñas coinciden
+            
             if (password !== confirmPassword) {
                 alert("Las contraseñas no coinciden. Por favor, inténtelo de nuevo.");
                 return;
             }
 
-            // Guardar el usuario en el localStorage
+            
             const newUser = { username, password };
             localStorage.setItem('user', JSON.stringify(newUser));
 
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('confirm-password').value = '';
         });
 
-        // Manejar el evento del botón "Mostrar contraseña" para registro
+       
         const showPasswordCheckbox = document.getElementById('register-show-password');
         if (showPasswordCheckbox) {
             showPasswordCheckbox.addEventListener('change', function() {
